92e2e32ca0f814f66e577efc97cf46e41061a35a
Pavel Baštinec <pavel4628@email.cz>
2023-10-08 21:21:38 +0200

    feature: about page introduction animation

--- src/app/pages/about/introduction/introduction.component.ts
+++ src/app/pages/about/introduction/introduction.component.ts
@@ -1,17 +1,51 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import {animate, state, style, transition, trigger} from '@angular/animations';
+import {SharedModule} from '../../../shared/shared.module';
 
 @Component({
   selector: 'app-introduction',
   templateUrl: './introduction.component.html',
   styleUrls: ['./introduction.component.scss'],
   standalone: true,
-    imports: [
-        MatIconModule,
-        MatButtonModule
-    ]
+  imports: [
+    MatIconModule,
+    MatButtonModule,
+    SharedModule
+  ],
+  animations: [
+    trigger('slideInRight', [
+      state('in', style({
+        left: 0,
+        opacity: 1,
+      })),
+      state('out', style({
+        left: '100px',
+        opacity: 0,
+      })),
+      transition('out => in', [
+        animate('.4s ease-out'),
+      ]),
+    ]),
+    trigger('slideInLeft', [
+      state('in', style({
+        right: 0,
+        opacity: 1,
+      })),
+      state('out', style({
+        right: '100px',
+        opacity: 0,
+      })),
+      transition('out => in', [
+        animate('.4s ease-out'),
+      ]),
+    ]),
+  ]
 })
 export class IntroductionComponent {
-
+  protected slideIn = 'out';
+  protected onAppear() {
+    this.slideIn = 'in';
+  }
 }
